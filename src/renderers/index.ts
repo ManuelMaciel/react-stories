5adb69c37ed32d16852d11f17e3e4319bf0c7eff
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-09-09 16:18:15 -0400

    add renderes

---
+++ src/renderers/index.ts
@@ -0,0 +1,6 @@
+import image from './Image';
+import video from './Video';
+import defaultRenderer from './Default';
+import autoplayContent from './AutoplayContent';
+
+export const renderers = [image, video, autoplayContent, defaultRenderer];
